28a347e6b48b132703db83d44bb7f9177b12927c
unknown <tanjirmahbub07@gmail.com>
2026-03-09 15:09:58 +0600

    Creating a priority and level functions to use them multiple times in code

--- JS/script.js
+++ JS/script.js
@@ -54,4 +54,60 @@ searchInput.addEventListener("input", function () {
   spinner.classList.add("hidden");
   displayIssues(data.data);
   
-};
+};// load cardData from API
+    // priority function
+function getPriorityBadge(priority) {
+  if (priority === "high") {
+    return `<div class="badge px-6 py-3 text-red-500 bg-red-100">High</div>`;
+  }
+
+  if (priority === "medium") {
+    return `<div class="badge px-6 py-3 text-yellow-500 bg-yellow-100">Medium</div>`;
+  }
+
+  if (priority === "low") {
+    return `<div class="badge px-6 py-3 text-gray-500 bg-gray-100">Low</div>`;
+  }
+}
+
+// lebels function
+function getLabels(labels) {
+  return labels
+    .map((label) => {
+      if (label === "bug") {
+        return `
+      <div class="badge border border-red-300 text-red-500 bg-red-100">
+        <i class="fa-solid fa-bug"></i> Bug
+      </div>`;
+      }
+
+      if (label === "enhancement") {
+        return `
+      <div class="badge border border-green-300 text-green-500 bg-green-100">
+        <i class="fa-solid fa-wand-magic-sparkles"></i> Enhancement
+      </div>`;
+      }
+
+      if (label === "documentation") {
+        return `
+      <div class="badge border border-blue-300 text-blue-500 bg-blue-100">
+        <i class="fa-brands fa-readme"></i> Documentation
+      </div>`;
+      }
+
+      if (label === "good first issue") {
+        return `
+      <div class="badge border border-purple-300 text-purple-500 bg-purple-100">
+        <i class="fa-solid fa-file-circle-exclamation"></i> Good first issue
+      </div>`;
+      }
+
+      if (label === "help wanted") {
+        return `
+      <div class="badge border border-yellow-300 text-yellow-500 bg-yellow-100">
+        <i class="fa-regular fa-circle-stop"></i> Help wanted
+      </div>`;
+      }
+    })
+    .join("");
+}
