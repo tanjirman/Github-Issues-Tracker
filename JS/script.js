e2723b10b7fa24738263c2f1e6c599c3ab7d17e0
unknown <tanjirmahbub07@gmail.com>
2026-03-09 15:08:40 +0600

    Adding search bar function

--- JS/script.js
+++ JS/script.js
@@ -18,4 +18,40 @@ tabButtons.forEach((button) => {
       displayIssues(filtered);
     }
   });
+});// search bar section
+const searchInput = document.getElementById("search-input");
+searchInput.addEventListener("input", function () {
+
+  const spinner = document.getElementById("loading-spinner");
+  spinner.classList.remove("hidden");
+
+  const searchText = searchInput.value.trim().toLowerCase();
+
+  // If input is empty, show all issues
+  if (!searchText) {
+    spinner.classList.add("hidden");
+    displayIssues(allIssues);
+    return;
+  }
+
+  // Filter issues only if match first word equals search text
+  const filtered = allIssues.filter((issue) => {
+    const firstWord = issue.title.split(" ")[0].toLowerCase();
+    return firstWord === searchText; // First word exact match
+  });
+
+  displayIssues(filtered);
+  spinner.classList.add("hidden");
 });
+
+
+// rendering data from API
+ const searchIssues = async () => {
+
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  spinner.classList.add("hidden");
+  displayIssues(data.data);
+  
+};
